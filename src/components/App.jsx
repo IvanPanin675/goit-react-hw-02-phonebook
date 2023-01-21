77828cfb58f63d627979aaa257409e6942358a20
IvanPanin675 <jovanni.94.van@gmail.com>
2023-01-21 16:56:33 +0200

    step-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  onHandleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.elements.name.value);
+    const name = e.target.elements.name.value;
+    const id = nanoid();
+    const contact = { id, name };
+    this.setState(({ contacts }) => ({ contacts: [ ...contacts, contact] }));
+  }
+
+  render() {
+    return (
+      <>
+      <form onSubmit={this.onHandleSubmit}>
+        <label htmlFor="">Name</label>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <button type='submit'>Add contact</button>
+        </form>
+        <div>
+          <p>Contacts</p>
+          <ul>
+            {this.state.contacts.map(({ id, name }) => { return <li key={id}>{ name}</li>})}
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
