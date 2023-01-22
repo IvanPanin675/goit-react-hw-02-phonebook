bcf3e6471a1014b687f5b504c6c3ccdc19aa31c5
IvanPanin675 <jovanni.94.van@gmail.com>
2023-01-22 15:03:21 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 
+import FormAddContact from './FormAddContact/FormAddContact';
+import FilterSearch from './FilterSearch/FilterSearch';
+import ContactsList from './ContactsList/ContactsList';
+
 export class App extends Component {
   state = {
     contacts: [
@@ -10,8 +14,6 @@ export class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
 
   onHandleSubmit = e => {
@@ -45,50 +47,28 @@ export class App extends Component {
     );
   };
 
+  onDeleting = deleteId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== deleteId),
+    }));
+  };
+
   render() {
-    const { filter } = this.state;
+    const { filter, contacts } = this.state;
     return (
       <>
-        <h3>Phonebook</h3>
-        <form onSubmit={this.onHandleSubmit}>
-          <label htmlFor="">Name</label>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <label htmlFor="">Number</label>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <button type="submit">Add contact</button>
-        </form>
-
-        <div>
-          <h3>Contacts</h3>
-          <p>Find contacts by name</p>
-          <input
-            onChange={this.onSearchName}
-            type="text"
-            name="filter"
-            value={filter}
-          />
-          <ul>
-            {this.state.contacts.map(({ id, name, number }) => {
-              return (
-                <li key={id}>
-                  {name}: {number}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <h1>Phonebook</h1>
+        <FormAddContact onHandleSubmit={this.onHandleSubmit} />
+        {Boolean(contacts[0]) && (
+          <>
+            <h2>Contacts</h2>
+            <FilterSearch onSearchName={this.onSearchName} value={filter} />
+            <ContactsList
+              getVisibleContacts={this.getVisibleContacts()}
+              onDelete={this.onDeleting}
+            />
+          </>
+        )}
       </>
     );
   }
